ee64644c134a47f40372a96b46c3c43f698d3d80
GymTech <gymtechbuisness@gmail.com>
2025-11-15 14:08:35 -0800

    fix(skins): import named products (build fix)

--- src/skins/ReplicaShop.jsx
+++ src/skins/ReplicaShop.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './replica.css'
-import products from '../data/products'
+import { products } from '../data/products'
 import { Link } from 'react-router-dom'
 
 export default function ReplicaShop(){
